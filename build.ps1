5ca04faa5d85d69e77161d6968068550410d3ff7
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-06-24 16:09:14 +0700

    Update

--- build.ps1
+++ build.ps1
@@ -1,6 +1,19 @@
 # $folder = "src\T01_intro"
-# $folder = "src\T02_js_ts"
-# $folder = "src\T03_pf_db"
+# npm run html $folder
+# npm run pdf $folder
+
+$folder = "src\T02_js_ts"
+npm run html $folder
+npm run pdf $folder
+
+$folder = "src\T03_pf_db"
+npm run html $folder
+npm run pdf $folder
+
 $folder = "src\T04_pf_backend"
+npm run html $folder
+npm run pdf $folder
+
+$folder = "src\T05_pf_frontend"
 npm run html $folder
 npm run pdf $folder
