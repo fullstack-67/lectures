4cf25bd1d6b71147b4ee49ce060eac2dce394a1b
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-10-03 11:05:58 +0700

    Update

--- build.ps1
+++ build.ps1
@@ -66,6 +66,6 @@ $folder = "src\T14_react_native"
 npm run html $folder
 npm run pdf $folder
 
-$folder = "src\T15_mobile_services"
-npm run html $folder
-npm run pdf $folder
+# $folder = "src\T15_mobile_services"
+# npm run html $folder
+# npm run pdf $folder
